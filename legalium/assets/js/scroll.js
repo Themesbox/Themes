1b77440d516ebadb2ea91f5891dd18fa42be621e
Themesbox <44608013+Themesbox@users.noreply.github.com>
2018-11-25 18:42:31 +0100

    Update scroll.js

--- legalium/assets/js/scroll.js
+++ legalium/assets/js/scroll.js
@@ -253,3 +253,15 @@ $.fn.easeScroll = function(options) {
     }();
 }
 
+$("html").easeScroll({
+  frameRate: 60,
+  animationTime: 1000,
+  stepSize: 120,
+  pulseAlgorithm: !0,
+  pulseScale: 8,
+  pulseNormalize: 1,
+  accelerationDelta: 20,
+  accelerationMax: 1,
+  keyboardSupport: !0,
+  arrowScroll: 50
+});
