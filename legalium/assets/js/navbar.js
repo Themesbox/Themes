1a24e9daf379454b6e099a6d8aa7e1d9dd31332b
Themesbox <44608013+Themesbox@users.noreply.github.com>
2018-11-26 18:19:51 +0100

    Update navbar.js

--- legalium/assets/js/navbar.js
+++ legalium/assets/js/navbar.js
@@ -1,16 +1,15 @@
 $(function() {
+    
     var navbar = $(".navbar");
     
     $(window).scroll(function() {
-    
-        var scroll = $(window).scrollTop();
         
+        var scroll = $(window).scrollTop();
         if (scroll >= 100) {
             navbar.removeClass('bg-transparent').addClass("bg-dark");
         } else {
             navbar.removeClass("bg-dark").addClass('bg-transparent');
-        }
+        } 
         
-    });
-    
+    }); 
 });
